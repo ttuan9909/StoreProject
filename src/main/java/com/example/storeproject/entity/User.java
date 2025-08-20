911910c3853eefa8bdd33d69495c8f00f6599f20
ttuan9909 <ttanhtuan2003@gmail.com>
2025-08-20 21:13:14 +0700

    tao entity

---
+++ src/main/java/com/example/storeproject/entity/User.java
@@ -0,0 +1,113 @@
+package com.example.storeproject.entity;
+
+import java.time.LocalDate;
+
+public class User {
+    private int userId;
+    private String userName;
+    private String password;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String address;
+    private String role;
+    private LocalDate createdAt;
+
+    private Integer positionId;
+
+    public User() {
+    }
+
+    public User(int userId, String userName, String password, String fullName, String email, String phone, String address, String role, LocalDate createdAt, Integer positionId) {
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.role = role;
+        this.createdAt = createdAt;
+        this.positionId = positionId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Integer getPositionId() {
+        return positionId;
+    }
+
+    public void setPositionId(Integer positionId) {
+        this.positionId = positionId;
+    }
+}
